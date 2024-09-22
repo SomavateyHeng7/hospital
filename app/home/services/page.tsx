e733aa44fe75d7b03f500b661915a0cdb2b8394e
panha roth <panharoth@Roths-Mac.local>
2024-09-22 22:03:10 +0700

    initial commit

---
+++ app/home/services/page.tsx
@@ -0,0 +1,19 @@
+import Service from "@/components/ui/Services/services_header";
+import Form from "@/components/ui/Services/services_form";
+
+export default function servicesPage(){
+    const services = [
+        {name: "Childbirth"},
+        {name:"Pharmacy"},
+        {name:"Chronic diseases"},
+        {name:"Primary care"},
+        {name:"Blood services"},
+        {name:"Diagnostic services"},
+    ];
+    return(
+        <div>
+            <Service />
+            <Form services = {services} />
+        </div>
+    );
+}
